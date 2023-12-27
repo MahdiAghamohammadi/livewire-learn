eecf575e40e5c390f579fdbc68ebdd70c1f88192
Mahdi Aghamohammadi <aghamohammadi.mehdi15@gmail.com>
2023-12-27 22:52:39 +0330

    connect to db and add crud

--- app/Http/Livewire/TaskComponent.php
+++ app/Http/Livewire/TaskComponent.php
@@ -2,25 +2,57 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Task;
+use App\Models\User;
 use Livewire\Component;
 
 class TaskComponent extends Component
 {
 
-    public string $name;
-
-    public string $task;
-    public array $tasks = [];
+    public $user;
+    public $task;
+    public $tasks;
+    public $editId = 0;
 
     public function addNewTask(): void
     {
-        $this->tasks[] = $this->task;
+        $newTask = Task::query()->create([
+            'task' => $this->task,
+            'user_id' => $this->user->id
+        ]);
         $this->task = null;
+        $this->tasks = Task::query()->where('user_id', $this->user->id)->get();
     }
 
-    public function mount($name): void
+    public function editTask($id)
     {
-        $this->name = $name;
+        $task = Task::find($id);
+        $this->task = $task->task;
+        $this->editId = $task->id;
+    }
+
+    public function updateTask()
+    {
+        if ($this->editId) {
+            $task = Task::find($this->editId);
+            $task->task = $this->task;
+            $task->save();
+            $this->tasks = Task::query()->where('user_id', $this->user->id)->get();
+            $this->task = null;
+            $this->editId = 0;
+        }
+    }
+
+    public function deleteTask($id)
+    {
+        Task::query()->where('id', $id)->delete();
+        $this->tasks = $this->tasks->where('id', '!=', $id);
+    }
+
+    public function mount($user)
+    {
+        $this->user = User::find($user);
+        $this->tasks = Task::query()->where('user_id', $user)->get();
     }
 
     public function render()
--- resources/views/livewire/task-component.blade.php
+++ resources/views/livewire/task-component.blade.php
@@ -1,14 +1,27 @@
 <div class="main_div">
     <div class="container">
-        <h1 class="text-center">{{$name}}</h1>
+        <h1 class="text-center">{{$user->name}}</h1>
 
         <div class="action_content mt-2">
-            <button class="btn btn-success mx-1" wire:click="addNewTask">add</button>
+            <button class="btn mx-1 {{ $editId ? 'btn-warning' : 'btn-success'}}"
+                    wire:click="{{ $editId ? 'updateTask' : 'addNewTask' }}">{{ $editId ? 'edit' : 'add'}}</button>
             <input type="text" class="from-control mx-1" wire:model="task">
         </div>
         <ul>
             @foreach($tasks as $task)
-                <li>{{$task}}</li>
+                <div class="d-flex flex-row justify-content-around">
+                    <div class="p-2">
+                        <button class="btn btn-sm rounded-circle btn-warning" wire:click="editTask({{ $task->id }})">
+                            edit
+                        </button>
+                        <button class="btn btn-sm rounded-circle btn-danger" wire:click="deleteTask({{ $task->id }})">
+                            delete
+                        </button>
+                    </div>
+                    <div class="p-2">
+                        <li>{{$task->task}}</li>
+                    </div>
+                </div>
             @endforeach
         </ul>
     </div>
