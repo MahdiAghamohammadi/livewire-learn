7cdb7b521b48de91e1fa37cc3e881bb4384bdbf3
Mahdi Aghamohammadi <mahdi.develoop@gmail.com>
2024-01-09 22:48:44 +0330

    added uploading image in post page

--- app/Http/Livewire/Posts.php
+++ app/Http/Livewire/Posts.php
@@ -4,29 +4,38 @@ namespace App\Http\Livewire;
 
 use App\Models\Post;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class Posts extends Component
 {
+    use WithFileUploads;
+
     public $topTitle = "Hello";
     public $isSuccess = true;
 
     public $title;
     public $content;
+    public $image;
+
     public $allPost;
+
     protected $rules = [
         'title' => 'required|string|min:5',
         'content' => 'required|string|max:100',
+        'image' => 'image|max:1024'
     ];
 
 
     public function savePost()
     {
         $this->validate();
+        $imgPath = $this->image->store('public/images');
         Post::create([
             'title' => $this->title,
             'content' => $this->content,
+            'image' => "/storage/images/" . explode("/", $imgPath)[2]
         ]);
-        $this->reset('title', 'content');
+        $this->reset('title', 'content', 'image');
         $this->allPost = Post::all();
     }
 
--- resources/views/livewire/posts.blade.php
+++ resources/views/livewire/posts.blade.php
@@ -2,10 +2,11 @@
     <div class="container">
         <h1 class="text-center">Title of page</h1>
         <h1 class="text-center {{ $isSuccess ? 'text-success' : 'text-danger' }}">{{ $topTitle }}</h1>
-        <form wire:keydown.enter.prevent="savePost">
+        <form wire:submit.prevent="savePost">
             <div class="mt-2">
                 {{-- <button class="btn btn-success mx-1" type="button" wire:click.stop="$set('topTitle', 'hi')">add</button> --}}
-                <button class="btn btn-success mx-1" type="button" wire:click.stop="$toggle('isSuccess')">add</button>
+                {{-- <button class="btn btn-success mx-1" type="button" wire:click.stop="$toggle('isSuccess')">add</button> --}}
+                <button class="btn btn-success mx-1" type="submit">add</button>
                 <input type="text" class="form-control mx-1" placeholder="title" wire:model.lazy='title'>
             </div>
             @error('title')
@@ -15,6 +16,10 @@
             @error('content')
                 <small class="text-danger text-center">{{ $message }}</small>
             @enderror
+            <input type="file" class="form-control mt-1" wire:model='image'>
+            @error('image')
+                <small class="text-danger text-center">{{ $message }}</small>
+            @enderror
         </form>
         <ul>
             @foreach ($allPost as $post)
@@ -22,6 +27,7 @@
                     <h3>{{ $post->title }}</h3>
                     <p>
                         {{ $post->content }}
+                        <img src="{{ $post->image }}" width="100" alt="">
                     </p>
                 </li>
             @endforeach
