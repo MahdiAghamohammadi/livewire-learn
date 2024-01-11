af520f3b71908549b51a5391e8bf1050ff89f269
Mahdi Aghamohammadi <mahdi.develoop@gmail.com>
2024-01-11 12:04:56 +0330

    added loader to post page

--- resources/views/livewire/posts.blade.php
+++ resources/views/livewire/posts.blade.php
@@ -1,7 +1,31 @@
 <div>
+    <style>
+        .loader {
+            border: 5px solid #f3f3f3;
+            /* Light grey */
+            border-top: 5px solid #3498db;
+            /* Blue */
+            border-radius: 50%;
+            width: 35px;
+            height: 35px;
+            animation: spin 2s linear infinite;
+            margin-top: 15px;
+        }
+
+        @keyframes spin {
+            0% {
+                transform: rotate(0deg);
+            }
+
+            100% {
+                transform: rotate(360deg);
+            }
+        }
+    </style>
     <div class="container">
         <h1 class="text-center">Title of page</h1>
         <h1 class="text-center {{ $isSuccess ? 'text-success' : 'text-danger' }}">{{ $topTitle }}</h1>
+        <div class="loader" wire:loading wire:target='savePost'></div>
         <form wire:submit.prevent="savePost">
             <div class="mt-2">
                 {{-- <button class="btn btn-success mx-1" type="button" wire:click.stop="$set('topTitle', 'hi')">add</button> --}}
